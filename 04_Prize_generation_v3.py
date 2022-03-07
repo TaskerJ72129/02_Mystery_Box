c698dfc6eccb1accdb6753f12a465d0586ccb5a4
James Tasker <taskerj72129@masseyhigh.school.nz>
2022-03-07 14:50:03 +1300

    04, 05

--- 04_Prize_generation_v3.py
+++ 04_Prize_generation_v3.py
@@ -12,20 +12,22 @@ for item in range(0, NUM_TRIALS):
     for thing in range(0, 3):
 
         # randint finds numbers between givin enpoints, including both endpoints
-        prize_num = random.randint(1,100)
+        prize_num = random.randint(1,1000)
         # prize += " "
-        if 0 < prize_num <= 5:
+        if 0 < prize_num <= 50:
             # one in ten chance of getting gold
             # prize += "gold"
             round_winnings += 5
-        elif 5 < prize_num <= 25:
+        elif 50 < prize_num <= 250:
             # get silver if it's a 2 or 3
             # prize += "silver"
             round_winnings += 2
-        elif 25 < prize_num <= 65:
+        elif 250 < prize_num <= 650:
             # copper if its 4, 5, 6, 7 
             # prize += "copper"
             round_winnings += 1
+        elif prize_num == 777:
+            round_winnings = 777
         '''else prize_num == 4:
             prize += "lead"'''
 
--- 05_Game _Playable.py
+++ 05_Game _Playable.py
@@ -45,32 +45,107 @@ class Game:
         self.game_frame.grid()
 
         # Heading Row
-        self.heading_label = Label(self.game_frame, text="Heading",
+        self.heading_label = Label(self.game_frame, text="Play...",
                                    font="Arial 24 bold", padx=10, pady=10)
         self.heading_label.grid(row=0)
 
-        # Balance Label
-        self.balance_frame = Frame(self.game_frame)
-        self.balance_frame.grid(row=1)
+        # Instructions Label
+        self.instructions_label = Label(self.game_frame, wrap=300, justify=LEFT,
+                                        text="Press <enter> or click the 'Open Boxes' button to reveal the contents of the mystery boxes.",
+                                        font="Arial 10", padx=10, pady=10)
+        self.instructions_label.grid(row=1)
 
-        self.balance_label = Label(self.game_frame, text="Balance...")
-        self.balance_label.grid(row=2)
+        # Boxes go here (row 2)
+        box_text = "Arial 16 bold"
+        box_back = "#b9ea96" # light green
+        box_width = 5
+        self.box_frame = Frame(self.game_frame)
+        self.box_frame.grid(row=2, pady=10)
 
-        self.play_button = Button(self.game_frame, text="Gain",
-                                  padx=10, pady=10, command=self.reveal_boxes)
+        self.prize1_label = Label(self.box_frame, text="?\n", font=box_text,
+                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize1_label.grid(row=0, column=0)
+
+        self.prize2_label = Label(self.box_frame, text="?\n", font=box_text,
+                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize2_label.grid(row=0, column=1, padx=10)
+
+        self.prize3_label = Label(self.box_frame, text="?\n", font=box_text,
+                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize3_label.grid(row=0, column=2)
+
+        # Play button goes here (row 3)
+        self.play_button = Button(self.game_frame, text="Open Boxes", bg="#FFFF33",
+                                  font="Arial 15 bold", width=20, padx=10, pady=10,
+                                  command=self.reveal_boxes)
         self.play_button.grid(row=3)
+
+        # Balance Label (row 4)
+
+        start_text = "Game Cost: ${} \n "" \nHow much will you win?".format(stakes * 5)
+
+        self.balance_label = Label(self.game_frame, font="Arial 12 bold", fg="green",
+                                   text=start_text, wrap=300, justify=LEFT)
+        self.balance_label.grid(row=4, pady=10)
+
+        # Help and Game Stats button (row 5)
+        self.help_export_frame = Frame(self.game_frame)
+        self.help_export_frame.grid(row=5, pady=10)
+
+        self.help_button = Button(self.help_export_frame, text="Help / Rules",
+                                  font="Arial 15 bold", bg="#808080", fg="white")
+        self.help_button.grid(row=0, column=1, padx=2)
 
     def reveal_boxes(self):
         # retrieve the balance from the initial function
         current_balance = self.balance.get()
+        stakes_multiplier = self.multiplier.get()
 
-        # Adjust the balance (subtract game cosr and add pay out)
-        # For testing purposes just add 2
-        current_balance += 2
+        round_winnings = 0
+        prizes = []
+        for item in range(0, 3):
+            prize_num = random.randint(1,100)
 
-        # Set balance to adjusted balance
+            if 0 < prize_num <=5:
+                prize = "gold\n(${})".format(5* stakes_multiplier)
+                round_winnings += 5 * stakes_multiplier
+
+            elif 5 < prize_num <=25:
+                prize = "silver\n(${})".format(2* stakes_multiplier)
+                round_winnings += 2 * stakes_multiplier
+            
+            elif 25 < prize_num <=65:
+                prize = "copper\n(${})".format(1* stakes_multiplier)
+                round_winnings += stakes_multiplier
+            else:
+                prize = "lead\n($0)"
+
+            prizes.append(prize)
+
+        # Display prizes
+        self.prize1_label.config(text=prizes[0])
+        self.prize2_label.config(text=prizes[1])
+        self.prize3_label.config(text=prizes[2])
+
+        # Deduct cost of game
+        current_balance -= 5 * stakes_multiplier
+
+        # Add Winnings
+        current_balance += round_winnings
+
+        # Set balance to new balance
         self.balance.set(current_balance)
         
-        # Edit label so user can see their balance
-        self.balance_label.configure(text="Balance: {}".format(current_balance))
 
+        balance_statement = "Game Cost: ${}\nPayback: ${} \n Current Balance: ${}".format(5 * stakes_multiplier,
+                                                                                          round_winnings,
+                                                                                          current_balance)
+        # Edit label so user can see their balance
+        self.balance_label.configure(text=balance_statement)
+
+
+# Main Routine
+if __name__ == "__main__":
+    root= Tk() 
+    something = Start(root)
+    root.mainloop()
